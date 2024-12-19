cc6d9043cbb75381f9be349b53424d343bb6124b
fabroche <kraxusmmo@gmail.com>
2024-12-19 16:37:30 +0000

    refactor: printing the error msg instead of the Error object

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
 
     if (isLoading) return <div>Loading...</div>
 
-    if (error) return <div>Error: {error}</div>
+    if (error) return <div>Error: {error.message}</div>
 
     if (!courses) return <div>Courses not Found</div>
 
